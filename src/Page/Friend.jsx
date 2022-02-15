f96c5d979e07225e0eac8381548e5dde510a48ee
Ananpiya134 <anan.piyapanee@gmail.com>
2022-02-15 13:44:53 +0700

    config axios localStorage

---
+++ src/Page/Friend.jsx
@@ -0,0 +1,11 @@
+function Friend() {
+
+    return (
+        <>
+            Friend
+        </>
+    );
+
+}
+
+export default Friend;
--- src/routes/RouteConfig.js
+++ src/routes/RouteConfig.js
@@ -2,6 +2,9 @@ import { Route, Routes } from 'react-router-dom';
 import PublicLayout from '../components/layout/PublicLayout';
 import Login from '../Page/Login';
 import Register from '../Page/Register';
+import MainLayout from '../components/layout/MainLayout';
+import Home from '../Page/Home'
+import Friend from '../Page/Friend';
 
 function RouteConfig() {
     return (
@@ -9,8 +12,13 @@ function RouteConfig() {
             <Route path="/" element={<PublicLayout />}>
                 <Route path="" element={<Login />} />
                 <Route path="register" element={<Register />} />
-
             </Route>
+
+            {/* <Route path="/" element={<MainLayout />}>
+                <Route path="" element={<Home />} />
+                <Route path="friend" element={<Friend />} />
+
+            </Route> */}
         </Routes>
     )
 }
